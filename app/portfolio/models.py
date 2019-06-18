0da8d7c9c899cf28ea52f595389cb7b71816deca
Petr Ospalý <petr@ospalax.cz>
2019-06-19 00:01:33 +0200

    Add data model
    Signed-off-by: Petr Ospalý <petr@ospalax.cz>

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=100)
+    def __str__(self):
+        return self.menu_item
+
+class Content(models.Model):
+    menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    markup = models.CharField(max_length=2000)
+    def __str__(self):
+        return self.menu_item + '_Content'
