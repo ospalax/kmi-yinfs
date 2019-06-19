f27a3376e698a581db9baf3190b6c8294cc315d4
Petr Ospalý <petr@ospalax.cz>
2019-06-19 12:43:28 +0200

    Fix superuser creation
    Try to create him only if it is not already there.
    
    Signed-off-by: Petr Ospalý <petr@ospalax.cz>

--- cmd.sh
+++ cmd.sh
@@ -15,12 +15,20 @@ if [ -n "$ADMIN_USERNAME" ] && \
     [ -n "$ADMIN_EMAIL" ] ;
 then
     echo "Create superuser: ${ADMIN_USERNAME}"
-    python manage.py shell -c "from django.contrib.auth.models import User; \
-        User.objects.create_superuser(\
-        '${ADMIN_USERNAME}', \
-        '${ADMIN_EMAIL}', \
-        '${ADMIN_PASSWORD}' \
-        )"
+    # if it not already exists...
+    if ! python manage.py shell -c "from django.contrib.auth.models import User; \
+        users = list(User.objects.values('username')); \
+        admin = [user['username'] for user in users if user['username'] == 'admin']; \
+        print(admin[0] if len(admin) > 0 else ''); \
+        " | grep -q "^${ADMIN_USERNAME}\$" ;
+    then
+        python manage.py shell -c "from django.contrib.auth.models import User; \
+            User.objects.create_superuser(\
+            '${ADMIN_USERNAME}', \
+            '${ADMIN_EMAIL}', \
+            '${ADMIN_PASSWORD}' \
+            )"
+    fi
 fi
 
 exec python manage.py runserver 0.0.0.0:8000
