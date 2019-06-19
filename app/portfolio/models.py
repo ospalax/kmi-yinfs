e0085223879c093169644acaf3c56ba480fd3082
Petr Ospalý <petr@ospalax.cz>
2019-06-19 03:38:37 +0200

    Add menu item order field to the model
    Signed-off-by: Petr Ospalý <petr@ospalax.cz>

--- app/portfolio/models.py
+++ app/portfolio/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Menu(models.Model):
     menu_item = models.CharField(max_length=100)
+    order = models.IntegerField('Menu item order', default=0)
     def __str__(self):
         return self.menu_item
 
