ac8128f083cdbc34f7781e01a17dbc1ea8a25344
Petr Ospalý <petr@ospalax.cz>
2019-06-19 04:20:03 +0200

    Add view for menu item content
    Signed-off-by: Petr Ospalý <petr@ospalax.cz>

--- app/portfolio/urls.py
+++ app/portfolio/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /portfolio/
     path('', views.index, name='index'),
+    # /portfolio/<menu_item>
+    path('<str:menu_item>/', views.detail, name='detail'),
 ]
--- app/portfolio/views.py
+++ app/portfolio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Hello, you are at my landing page.</h1>")
+
+def detail(request, menu_item):
+    return HttpResponse("<h2>Content for menu item %s.</h2>" % menu_item)
